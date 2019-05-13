cc130cf3e93e0a8b0188a6688b87a1edb617da7c
Jamilah McWilliams <jamilah.mcwilliams@flatironschool.com>
2019-05-13 22:38:05 +0000

    TESTS PASS-DONE

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 
 export default class ColorBox extends Component {
-  
+
+  //a traditional if statement from JavaScript will not work in this context
+  //Ternary operator will work in this instance, statements must return a value
+
   render() {
     return (
-      <div className="color-box" style={{opacity: 2}}>
-        {/* your conditional code here! */}
+      <div className="color-box" style={{opacity: this.props.opacity}}>
+        {this.props.opacity >= 0.2 ? <ColorBox opacity={this.props.opacity -0.1} /> : null}
       </div>
     )
   }
-  
+
 }
