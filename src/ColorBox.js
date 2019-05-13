d1abd9b708eadf8a4a42399fe9bfcf6457aaf4c4
Jamilah McWilliams <jamilah.mcwilliams@flatironschool.com>
2019-05-13 22:06:26 +0000

    Automatically backed up by Learn

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -8,6 +8,7 @@ export default class ColorBox extends Component {
   render() {
     return (
       <div className="color-box" style={{opacity: 2}}>
+      debugger
         {this.props.opacity >= 0.2 ? <ColorBox opacity={this.props.opacity -0.1} /> : null}
       </div>
     )
